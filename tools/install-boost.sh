489834f7037c8c8fb7f36f944df9e1550ff34383
Ben Lindsay <benjlindsay@gmail.com>
2018-06-25 11:38:02 -0400

    cleaned up boost install

--- tools/install-boost.sh
+++ tools/install-boost.sh
@@ -4,20 +4,38 @@
 #
 # Copyright (c) 2018 Ben Lindsay <benjlindsay@gmail.com>
 
-PROJ_ROOT=$(pwd)
-BOOST_VERSION=boost_1_67_0
-TARBALL=$BOOST_VERSION.tar.bz2
+proj_root="$(pwd)"
+major="1"
+minor="67"
+patch="0"
+version="$major.$minor.$patch"
+boost_dir="boost_"$major"_"$minor"_"$patch
+tarball="$boost_dir.tar.bz2"
+url="https://dl.bintray.com/boostorg/release/$version/source/$tarball"
 
-if [ ! -f /tmp/$TARBALL ]; then
-  wget -O /tmp/$TARBALL https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
+if [ ! -f /tmp/$tarball ]; then
+  echo "downloading $url"
+  if [ $(command -v wget) ]; then
+    wget -O /tmp/$tarball $url
+  elif [ $(command -v curl) ]; then
+    curl -o /tmp/$tarball $url
+  fi
+else
+  echo "/tmp/$tarball already downloaded."
 fi
 
-rm -rf /tmp/$BOOST_VERSION
+echo "Removing /tmp/$boost_dir..."
+rm -rf /tmp/$boost_dir
 cd /tmp
-tar -xjf $TARBALL
-cd $BOOST_VERSION
-./bootstrap.sh --prefix=$PROJ_ROOT --with-libraries=filesystem
+echo "Extracting /tmp/$tarball..."
+tar -xjf $tarball
+cd $boost_dir
+echo "Running './bootstrap.sh'..."
+./bootstrap.sh --prefix=$proj_root --with-libraries=filesystem
+echo "Running './b2 install'..."
 b2_output=$(./b2 install)
-echo $b2_output | head -50
-echo "...\nSkipping a ton of lines of './b2 install' output"
-echo $b2_output | tail -50
+echo "$b2_output" | head -50
+echo "...\nSkipping a ton of lines of './b2 install' output...\n..."
+echo "$b2_output" | tail -50
+echo "Removing *.so* and *.dylib files..."
+rm $proj_root/lib/libboost*.so* $proj_root/lib/libboost*.dylib
